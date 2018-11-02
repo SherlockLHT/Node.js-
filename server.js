874896db77d042e77eda5a347f9520f27025c353
Sherlock <sherlock_lin@163.com>
2018-11-02 23:14:58 +0800

    update

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 //本地变量和模块取一样的名字是惯例
 var http = require("http");//请求http模块加载
 var url = require('url');
-var router = require('./router');
 
-function startServer() {
+function startServer(route) {
     //createServer是http模块的方法，执行后返回对象，有一个listen的方法，用http服务器监听端口号
     http.createServer(onRequest).listen(8080);
     console.log("Serve has started...");
 
     function onRequest(request, response) {
-        var pathName = url.parse(request.url).pathName;
+        var pathName = url.parse(request.url).pathname;
         console.log("Request for " + pathName + " received.");
 
-        router.route(pathName);
+        route(pathName);
 
         //200表示HTTP状态，后面参数表示HTTP头内容
         response.writeHead(200, { "Content-Type": "text/plain" });
