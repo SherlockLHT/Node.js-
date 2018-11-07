52718155ca4149d5bddf5ba65fbc0d6b19a9cf96
sherlock <sherlock_lin@163.com>
2018-11-07 09:09:06 +0800

    add .

--- 11/requestHandlers.js
+++ 11/requestHandlers.js
@@ -1,13 +1,20 @@
 var exec = require("child_process").exec;
 
+function sleep(milliSeconds){
+    var startTime = new Date().getTime();
+    while(new Date().getTime() < startTime + milliSeconds);
+}
+
 function start(){
     console.log("Request handler [start] was called.");
     var content = "empty";
 
+    sleep(5000);
+
     //非阻塞式
-    exec("find /", function(error, stdout, stderr){
-        content = stdout;
-    });
+    // exec("find /", function(error, stdout, stderr){
+    //     content = stdout;
+    // });
     return content;
 }
 
